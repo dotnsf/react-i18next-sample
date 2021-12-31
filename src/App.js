6931286e58dfa0d2c90023e7fad6057595babe1a
K.Kimura <dotnsf@gmail.com>
2022-01-01 00:02:47 +0900

    rebuild app with external image

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://raw.githubusercontent.com/dotnsf/react-i18next-sample/gh-pages/logo.svg" className="App-logo" alt="logo" />
         <p>
           {t('edit_message1')} <code>src/App.js</code> {t('edit_message2')}
         </p>
